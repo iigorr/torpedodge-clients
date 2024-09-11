c6bbac8ecf49ad0fc580da06263dade70a8d754e
Julien Midedji <Julien.Midedji@gmail.com>
2024-09-11 21:31:43 +0200

    Add pipeline and sample bot with image

---
+++ example/go.mod
@@ -0,0 +1,13 @@
+module github.com/rebirth-in-ruins/torpedodge-clients/example
+
+go 1.23.1
+
+require (
+	github.com/coder/websocket v1.8.12
+	github.com/rebirth-in-ruins/torpedodge/server v0.0.0-20240911182554-32ca9862c78d
+)
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
---
+++ example/main.go
@@ -0,0 +1,85 @@
+// Package main implements a bot that circles around and drops a bomb occasionally.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"math/rand/v2"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+	"github.com/rebirth-in-ruins/torpedodge/server/game"
+)
+
+const (
+	urlStr = "wss://gameserver.resamvi.io/play"
+)
+
+var (
+	directions = []string{"LEFT", "BOMBLEFT", "DOWN", "DOWN", "RIGHT", "RIGHT", "UP", "UP"}
+
+	name = []string{
+		"Ella Atkinson",
+		"Duke Krueger",
+		"Kamari Solis",
+		"Ronin Santana",
+		"Myra Fuller",
+		"Andre Montes",
+		"Roselyn Frederick",
+		"Kase Gregory",
+		"Alaya Herman",
+		"Juelz Frost",
+		"Paula Reyna",
+		"Reginald Kirby",
+		"Skyla Holt",
+		"Niko Garrison",
+		"Cadence Graham",
+	}
+)
+
+func main() {
+	for {
+		_ = run() // When we died and lost connection, retry again.
+	}
+}
+
+func run() error {
+	conn, _, err := websocket.Dial(context.Background(), urlStr, nil)
+	if err != nil {
+		return fmt.Errorf("could not dial: %w", err)
+	}
+	defer conn.CloseNow()
+
+	// Join as player
+	name := name[rand.IntN(len(name))]
+	err = wsjson.Write(context.Background(), conn, "JOIN "+name)
+	if err != nil {
+		return fmt.Errorf("could not write: %w", err)
+	}
+
+	i := 0
+
+	for {
+		// RECEIVE NEXT STATE
+		var state game.GameStateResponse
+		err = wsjson.Read(context.Background(), conn, &state)
+		if err != nil {
+			return fmt.Errorf("could not write: %w", err)
+		}
+
+		// Sail in a circle
+		action := directions[i % len(directions)]
+
+		slog.Info(action)
+
+		// SEND ACTION
+		err = wsjson.Write(context.Background(), conn, action)
+		if err != nil {
+			return fmt.Errorf("could not write: %w", err)
+		}
+
+		i++
+	}
+}
+
